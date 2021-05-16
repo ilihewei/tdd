812fe845df9c7c93150c595db2f321f29fd6ffae
apple <1968895380@qq.com>
2021-05-16 15:27:12 +0800

    重构-优化的提交每当一次需求完成，就优化， 优化目标---代码的公共部分抽出来 1。引入testng @author lihewei @Date 2021-05-16 15：17

--- src/test/java/com/tdd/tddjava/ch04itctaccode/ShipSpec.java
+++ src/test/java/com/tdd/tddjava/ch04itctaccode/ShipSpec.java
@@ -4,17 +4,29 @@ import com.tdd.tddjava.ch04tictaccode.Direction;
 import com.tdd.tddjava.ch04tictaccode.Location;
 import com.tdd.tddjava.ch04tictaccode.Point;
 import com.tdd.tddjava.ch04tictaccode.Ship;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
 @Test
 public class ShipSpec {
- public void whenInstantedThenLocationisSet(){
-     Location location = new Location(new Point(21,13), Direction.NONE);
 
-        Ship ship = new Ship(location);
-        assertEquals(ship.getLocation(),location);
+
+    private Ship ship;
+    private Location location;
+
+    @BeforeMethod
+    public void beforeTest() {
+        location = new Location(new Point(21, 13), Direction.NORTH);
+        ship = new Ship(location);
+    }
+
+    public void whenInstantedThenLocationisSet() {
+        //Location location = new Location(new Point(21, 13), Direction.NONE);
+
+       // Ship ship = new Ship(location);
+        assertEquals(ship.getLocation(), location);
     }
 
 }
