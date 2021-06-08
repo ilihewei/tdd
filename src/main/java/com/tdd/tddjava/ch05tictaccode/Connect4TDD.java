51a93488727ac4949f9ebb11308ee7a86474a7e0
apple <1968895380@qq.com>
2021-06-08 19:30:33 +0800

    add pom 的测试框架
    @author lihewei
    @Date 2021-06-07 10：27

--- src/main/java/com/tdd/tddjava/ch05tictaccode/Connect4TDD.java
+++ src/main/java/com/tdd/tddjava/ch05tictaccode/Connect4TDD.java
@@ -1,10 +1,58 @@
 package com.tdd.tddjava.ch05tictaccode;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 /**
  * @author apple
  */
 public class Connect4TDD {
-    public int getNumberOfDiscs() {
+    private  static  final  int ROWS=6;
+
+    private  static  final int COLUMNS=7;
+
+    private  static  final String EMPTY=" ";
+
+    private String[][] board=new String[ROWS][COLUMNS];
+
+    public  Connect4TDD(){
+        for (String[] row:board){
+            Arrays.fill(row,EMPTY);
+        }
+    }
+
+    public int getNumberOfDiscs(){
+        return IntStream.range(0,COLUMNS).map(this::putDiscInColumn).sum();
+    }
+
+    private   int getNumberOfDiscsInColumn(int column){
+        return (int)IntStream.range(0,ROWS).filter(row->!EMPTY.equals(board[row][column])).count();
+    }
+
+    public  int putDiscInColumn(int column){
+        checkColumn(column);
+        int row=getNumberOfDiscsInColumn(column);
+        checkPositionToInsert(row,column);
+        board[row][column]="X";
+        return row;
+
+    }
+
+    private  void checkColumn(int column){
+        if(column<0||column>=COLUMNS){
+            throw  new RuntimeException("Invalid column"
+            +column);
+        }
+    }
+
+    private  void checkPositionToInsert(int row,int column){
+        if(row==ROWS){
+            throw  new RuntimeException("No more room in column"+column);
+        }
+    }
+
+
+    public long getCurrentPlayer() {
         return 0;
     }
 }
--- src/test/java/com/tdd/tddjava/ch05itctaccode/Connect4TDDSpec.java
+++ src/test/java/com/tdd/tddjava/ch05itctaccode/Connect4TDDSpec.java
@@ -1,21 +1,91 @@
 package com.tdd.tddjava.ch05itctaccode;
 
 import com.tdd.tddjava.ch05tictaccode.Connect4TDD;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-import static org.testng.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 
 public class Connect4TDDSpec {
     private Connect4TDD tested;
 
-    @BeforeMethod
+    @Rule
+    public ExpectedException expection = ExpectedException.none();
+
+
+    @Before
     public void beforeEachTest(){
         tested=new Connect4TDD();
     }
 
     @Test
     public void whenThenGameStartedTheBoardIsEmpty(){
-        assertEquals(tested.getNumberOfDiscs(),0);
+        assertThat(tested.getNumberOfDiscs(),is(0));
     }
+
+    @Test
+    public void whenDiscOutsideBoardThenRuntimeException(){
+        int column=-1;
+        expection.expect(RuntimeException.class);
+        expection.expectMessage("Invalid column"+column);
+        tested.putDiscInColumn(column);
+    }
+
+    @Test
+    public  void whenFirstInsertedColumnPositionIsZero(){
+        int column=1;
+        assertThat(tested.putDiscInColumn(column),is(0));
+    }
+
+    @Test
+    public  void  whenSecondDiscInsertedInColumnThenPositionIsOne(){
+        int column=1;
+        tested.putDiscInColumn(column);
+        assertThat(tested.putDiscInColumn(column),is(0));
+    }
+
+    @Test
+    public void whenSecondDiscInseredInColumnPositionIsOne(){
+        int column=1;
+        tested.putDiscInColumn(column);
+        assertThat(tested.putDiscInColumn(column),is(1));
+    }
+
+    @Test
+    public void whenDiscInsertedThenNumberOfDiscsIncreases(){
+        int column=1;
+        tested.putDiscInColumn(column);
+        assertThat(tested.getNumberOfDiscs(),is(1));
+    }
+
+    @Test
+    public void whenNoMoreRoomColumnThenRunTimeException(){
+        int column=1;
+        int maxDiscsInColumn=6;//the number of rows
+        for (int times = 0; times<maxDiscsInColumn ; times++) {
+            tested.putDiscInColumn(column);
+        }
+
+        expection.expect(RuntimeException.class);
+        expection.expectMessage("No more room in column"+column);
+        tested.putDiscInColumn(column);
+    }
+
+
+    @Test
+    public void whenFistPlaysThenDiscColorIsRed(){
+        assertThat(tested.getCurrentPlayer(),is("R"));
+    }
+
+    @Test
+    public void whenSecondPlaysThenDiscColorIsRed(){
+        int column=1;
+        tested.putDiscInColumn(column);
+        assertThat(tested.getCurrentPlayer(),is("R"));
+    }
+
 }
